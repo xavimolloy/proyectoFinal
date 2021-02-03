49836786b1bd9321473d77d73821fe1af1215653
@xavi.molloy <xavi.molloy@gmail.com>
2021-02-03 18:47:24 +0100

    first commit test

---
+++ app/src/main/java/com/example/enteratebien/AccesoActivity.java
@@ -0,0 +1,15 @@
+package com.example.enteratebien;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+public class AccesoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acceso);
+    }}
---
+++ app/src/main/java/com/example/enteratebien/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.enteratebien;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button GoToRegister = findViewById(R.id.GoToRegister);
+        Button GoToAccess = findViewById(R.id.GoToAccess);
+
+        GoToAccess.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AccesoActivity.class );
+            startActivity(intent);
+        });
+
+        GoToRegister.setOnClickListener( v -> {
+            Intent intent = new Intent(this, RegistroActivity.class);
+            startActivity(intent);
+
+        });
+    }
+}
